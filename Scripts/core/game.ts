4a6bc4d095671e4a5e6983c7805d5ed6288e0a51
YaroslavKabanov <ya.kabanov@gmail.com>
2016-04-14 22:20:58 -0400

    added gameover screen

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -83,7 +83,10 @@ var manifest = [
     { id: "ExitButton", src: "../../Assets/images/ExitButton1.png" },
     { id: "BackButton", src: "../../Assets/images/BackButton.png" },
     { id: "InstructionImage", src: "../../Assets/images/instruction.png" },
-    { id: "menuBackground", src: "../../Assets/images/maxresdefault1.jpg"}
+    { id: "menuBackground", src: "../../Assets/images/maxresdefault1.jpg"},
+    { id: "gameover", src: "../../Assets/images/gameover.jpg"},
+    { id: "mainmenu", src: "../../Assets/images/mainmenu.png"}
+    
 ];
 
 function preload(): void {
